429c1fd12b9dc402e3353d1c4dc6c966021e4a1a
Pavel Dorovskoy <pavel@ellis.appliedgeosolutions.com>
2013-09-05 14:13:32 -0400

    Added a few classes

--- b2tob3.py
+++ b2tob3.py
@@ -8,12 +8,17 @@
 # Use at your on risk, try it on a copy first!
 import os
 import re
+import sys
+from optparse import OptionParser
 
 # List of regular expression replacements tubles to be executed in order.
 # Does not include all changes from:
 # http://getbootstrap.com/getting-started/#migration
+
 regexes = [
-    (re.compile(r'\bspan([1-9]+)\b'), 'col-md-\\1'),
+    (re.compile(r'\bspan(\d+)\b'), 'col-md-\\1'),
+    (re.compile(r'\boffset(\d+)\b'), 'col-offset-\\1'),
+    (re.compile(r'\bicon-(\w+)\b'), '.glyphicon .glyphicon-\\1'),
     (re.compile(r'\bhero\-unit\b'), 'jumbotron'),
 
     (re.compile(r'\b(container|row)-fluid\b'), '\\1'),
@@ -28,13 +33,26 @@ regexes = [
     (re.compile(r'\bunstyled\b'), 'list-unstyled')
 ]
 
-pwd = os.path.abspath(os.curdir)
-for root, dirs, files in os.walk(pwd):
-    for f in files:
-        fname = os.path.join(root, f)
-        with open(fname, 'r') as curr_file:
-            content = curr_file.read()
-        with open(fname, 'w') as curr_file:
-            for regex in regexes:
-                content = re.sub(regex[0], regex[1], content)
-            curr_file.write(content)
+if __name__ == '__main__':
+    parser = OptionParser()
+
+    parser.add_option("-d", "--directory", dest="pwd",
+                  help="Directory to search", metavar="DIR", default=os.curdir)
+
+    parser.add_option("-e", "--extension", dest="ext",
+                  help="Extension of files to parse", metavar="EXT", default="html")
+
+    (options, args) = parser.parse_args()
+
+    pwd = os.path.abspath(options.pwd)
+    sys.exit()
+    for root, dirs, files in os.walk(pwd):
+        for f in files:
+            if f.endswith("."+options.ext)
+            fname = os.path.join(root, f)
+            with open(fname, 'r') as curr_file:
+                content = curr_file.read()
+            with open(fname, 'w') as curr_file:
+                for regex in regexes:
+                    content = re.sub(regex[0], regex[1], content)
+                curr_file.write(content)
